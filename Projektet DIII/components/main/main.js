10c72a00b5b59032af65494d84f06ee8e914575a
Hampus833 <hampuse266@gmail.com>
2024-04-23 13:34:08 +0200

    fixade instance data och posters

--- Projektet DIII/components/main/main.js
+++ Projektet DIII/components/main/main.js
@@ -1,6 +1,7 @@
-function renderMain(parentID) {
+async function renderMain(parentID, instanceData) {
+
     let mainContainer = document.createElement("div");
-    mainContainer.id = "mainContainer"; 
+    mainContainer.id = "mainContainer";
     parentID.append(mainContainer);
 
     let pDom = document.createElement("p");
@@ -14,11 +15,28 @@ function renderMain(parentID) {
     for (let i = 1; i <= 10; i++) {
         let photoDiv = document.createElement("div");
         photoDiv.id = "photo" + i;
+        let poster = document.createElement("img");
+        photoDiv.append(poster);
+        let randomNumber = getRandomPoster(instanceData.MOVIES);
+        console.log(randomNumber);
+        poster.src = instanceData.MOVIES[randomNumber].poster;
         mainBox.append(photoDiv);
     }
+
+
 
     let button = document.createElement("button");
     button.id = "mainButton";
     button.textContent = "Regenerate";
     mainContainer.append(button);
+}
+
+function getRandomPoster(movies) {
+    // console.log(movies);
+    const randomIndex = getRandomNumber(movies.length);
+    return randomIndex;
+}
+
+function getRandomNumber(max, min = 0) {
+    return min + Math.floor(max * Math.random());
 }
